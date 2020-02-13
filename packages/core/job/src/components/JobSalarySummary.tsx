3dbcb736b686f0577bfe97eaad923cc8b65acf41
Andrea Vanini <andrea.vanini@uidu.org>
2020-02-13 09:31:58 +0100

    Treemap or radial

--- packages/core/job/src/components/JobSalarySummary.tsx
+++ packages/core/job/src/components/JobSalarySummary.tsx
@@ -9,7 +9,7 @@ am4core.useTheme(am4themesAnimated);
 am4core.options.commercialLicense = true;
 
 export default class JobSalarySummary extends PureComponent<any> {
-  private chart: am4charts.TreeMap = undefined;
+  private chart: am4charts.PieChart = undefined;
   private uuid = uuid();
 
   componentDidMount() {
@@ -23,7 +23,8 @@ export default class JobSalarySummary extends PureComponent<any> {
   drawChart = () => {
     const { mySalary } = this.props;
     if (!this.chart) {
-      const chart = am4core.create(this.uuid, am4charts.TreeMap);
+      const chart = am4core.create(this.uuid, am4charts.PieChart);
+      // const chart = am4core.create(this.uuid, am4charts.TreeMap);
       chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
       chart.paddingTop = 5;
       chart.paddingLeft = 5;
@@ -41,41 +42,44 @@ export default class JobSalarySummary extends PureComponent<any> {
           value: 300,
         },
       ];
-      //  chart.radius = am4core.percent(70);
-      //  chart.innerRadius = am4core.percent(40);
-      //  chart.startAngle = 180;
-      //  chart.endAngle = 360;
+      chart.radius = am4core.percent(70);
+      chart.innerRadius = am4core.percent(40);
+      chart.startAngle = 180;
+      chart.endAngle = 360;
 
-      chart.dataFields.value = 'value';
-      chart.dataFields.name = 'country';
+      // chart.dataFields.value = 'value';
+      // chart.dataFields.name = 'country';
 
-      var level1 = chart.seriesTemplates.create('0');
-      level1.tooltip.disabled = true;
+      //  var level1 = chart.seriesTemplates.create('0');
+      //  level1.tooltip.disabled = true;
 
-      var level1_column = level1.columns.template;
-      level1_column.column.cornerRadius(10, 10, 10, 10);
-      level1_column.fillOpacity = 0.8;
-      level1_column.stroke = am4core.color('#f8f9fa');
-      level1_column.strokeWidth = 10;
-      level1_column.strokeOpacity = 1;
+      //  var level1_column = level1.columns.template;
+      //  level1_column.column.cornerRadius(10, 10, 10, 10);
+      //  level1_column.fillOpacity = 0.8;
+      //  level1_column.stroke = am4core.color('#f8f9fa');
+      //  level1_column.strokeWidth = 10;
+      //  level1_column.strokeOpacity = 1;
 
-      let level1_bullet = level1.bullets.push(new am4charts.LabelBullet());
-      level1_bullet.locationY = 0.5;
-      level1_bullet.locationX = 0.5;
-      level1_bullet.label.text = '{name}\n{value}';
-      level1_bullet.label.fill = am4core.color('#fff');
+      //  let level1_bullet = level1.bullets.push(
+      //    new am4charts.LabelBullet(),
+      //  );
+      //  level1_bullet.locationY = 0.5;
+      //  level1_bullet.locationX = 0.5;
+      //  level1_bullet.label.text = '{name}\n{value}';
+      //  level1_bullet.label.fill = am4core.color('#fff');
 
-      // let series = chart.series.push(new am4charts.PieSeries());
-      //  series.slices.template.cornerRadius = 10;
-      //  series.slices.template.innerCornerRadius = 7;
-      //  series.slices.template.draggable = true;
-      //  series.slices.template.inert = true;
-      //  series.alignLabels = false;
+      let series = chart.series.push(new am4charts.PieSeries());
+      series.dataFields.category = 'country';
+      series.dataFields.value = 'value';
+      series.slices.template.cornerRadius = 10;
+      series.slices.template.innerCornerRadius = 7;
+      series.slices.template.draggable = true;
+      series.slices.template.inert = true;
+      series.alignLabels = false;
 
-      //  series.hiddenState.properties.startAngle = 90;
-      //  series.hiddenState.properties.endAngle = 90;
+      series.hiddenState.properties.startAngle = 90;
+      series.hiddenState.properties.endAngle = 90;
 
-      // chart.legend = new am4charts.Legend();
       this.chart = chart;
     }
   };
