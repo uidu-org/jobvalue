0fee072beaaa5ef97492f420d98e805c0211417b
Andrea Vanini <andrea.vanini@uidu.org>
2020-02-21 15:46:59 +0100

    docs(changeset): Release billing form

---
+++ .changeset/popular-paws-work.md
@@ -0,0 +1,7 @@
+---
+'@jobvalue/billing': patch
+'@jobvalue/themes-stipendiogiusto': patch
+'@jobvalue/website': patch
+---
+
+Release billing form
---
+++ packages/ecommerce/billing/examples/Basic.tsx
@@ -0,0 +1,11 @@
+import Form from '@uidu/form';
+import React from 'react';
+import Billing from '..';
+
+export default function Basic() {
+  return (
+    <Form handleSubmit={() => {}} footerRenderer={() => {}}>
+      <Billing />
+    </Form>
+  );
+}
